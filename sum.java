5b0fb287ad35ed3d2dbc42161b999a18fd13c532
Samudrala Venkata Pavan Tarun Kumar <2200031955@kluniversity.in>
2025-01-24 16:16:43 +0530

    Initial Commit

---
+++ sum.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main{
+	public static void main(String[] args){
+		int a = 10,b = 20;
+		int sum = a + b;
+		System.out.println("Sum of "+a+" and "+b+" is "+sum);
+	}
+}
